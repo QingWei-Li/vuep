58682641ff8620e1eabe3f366a727729c631c394
wemlion <angusfu1126@qq.com>
2017-11-02 21:09:10 +0800

    feat(preview): support for mutiple selectors

--- src/components/preview.js
+++ src/components/preview.js
@@ -19,7 +19,7 @@ export default {
   computed: {
     scopedStyle () {
       return this.styles
-        ? this.styles.replace(/([\.#\w]+\w*?\s?{)/g, `.${this.className} $1`)
+        ? insertScope(this.styles, `.${this.className}`)
         : ''
     }
   },
@@ -55,4 +55,11 @@ export default {
       }
     }
   }
+}
+
+function insertScope (style, scope) {
+  const regex = /(^|\})\s*([^{]+)/g
+  return style.trim().replace(regex, (m, g1, g2) => {
+    return g1 ? `${g1} ${scope} ${g2}` : `${scope} ${g2}`
+  })
 }
